e0c8c6fd212541d700c3aa49e928ca3c7f429a84
William Vandebelt <wvandebelt@salesforce.com>
2023-04-01 12:28:31 -0400

    :rocket: redis cache

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/site-tech/jaw-platform
 go 1.19
 
 require (
+	ariga.io/entcache v0.1.0
 	entgo.io/contrib v0.3.4
 	entgo.io/ent v0.11.5-0.20221031135557-521f9b57bc3d
 	github.com/99designs/gqlgen v0.17.20
+	github.com/go-redis/redis/v8 v8.11.5
 	github.com/hashicorp/go-multierror v1.1.1
 	github.com/joho/godotenv v1.4.0
 	github.com/lib/pq v1.10.7
@@ -19,8 +21,11 @@ require (
 	github.com/agext/levenshtein v1.2.1 // indirect
 	github.com/agnivade/levenshtein v1.1.1 // indirect
 	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-openapi/inflect v0.19.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
@@ -28,6 +33,7 @@ require (
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/hashicorp/hcl/v2 v2.13.0 // indirect
 	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
+	github.com/mitchellh/hashstructure v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/urfave/cli/v2 v2.8.1 // indirect
--- server.go
+++ server.go
@@ -6,11 +6,20 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 
+	"ariga.io/entcache"
+	"entgo.io/contrib/entgql"
+	"entgo.io/ent/dialect"
+	"entgo.io/ent/dialect/sql"
+	"github.com/go-redis/redis/v8"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"github.com/vektah/gqlparser/v2/ast"
 
+	"github.com/99designs/gqlgen/graphql"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/site-tech/jaw-platform/ent"
@@ -18,6 +27,9 @@ import (
 )
 
 const defaultPort = "8880"
+
+var entCacheDriver *entcache.Driver
+var ttl time.Duration = time.Minute * 1
 
 func init() {
 	if err := godotenv.Load(); err != nil {
@@ -31,13 +43,17 @@ func main() {
 		port = defaultPort
 	}
 
-	var entOptions []ent.Option
-	entOptions = append(entOptions, ent.Debug())
-
-	client, err := ent.Open("postgres", getDSN(os.Getenv("DATABASE_URL")), entOptions...)
+	// set TTL
+	t, err := strconv.ParseInt(os.Getenv("CACHE_TTL"), 10, 0)
 	if err != nil {
-		log.Fatalf("failed opening connection to postgres: %v", err)
+		log.Println("cache_ttl env var err: ", err)
+	} else {
+		ttl = time.Duration(t) * time.Minute
 	}
+
+	// create cached ent client
+	ctx := context.Background()
+	client := createCachedEntClient(ctx)
 	defer client.Close()
 
 	// Run the auto migration tool.
@@ -48,9 +64,26 @@ func main() {
 	}
 
 	srv := handler.NewDefaultServer(graph.NewSchema(client))
+	srv.Use(entgql.Transactioner{TxOpener: client})
+	//gql-srv-response-middleware: context-level caching
+	srv.AroundResponses(func(ctx context.Context, next graphql.ResponseHandler) *graphql.Response {
+		if op := graphql.GetOperationContext(ctx).Operation; op != nil && op.Operation == ast.Query {
+			ctx = entcache.NewContext(ctx)
+		}
+		return next(ctx)
+	})
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
+	// endpoint for chache stats
+	http.HandleFunc("/stats", func(w http.ResponseWriter, r *http.Request) {
+		if entCacheDriver != nil {
+			stat := entCacheDriver.Stats()
+			fmt.Fprintf(w, "cache stats (gets: %d, hits: %d, errors: %d)\n", stat.Gets, stat.Hits, stat.Errors)
+		} else {
+			fmt.Fprintln(w, "cache mode is not enabled")
+		}
+	})
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
@@ -75,4 +108,42 @@ func getDSN(url string) string {
 	dbname = s4[1]
 
 	return fmt.Sprintf(dsnFormat, host, user, word, dbname, port)
+}
+
+func createCachedEntClient(ctx context.Context) *ent.Client {
+	return ent.NewClient([]ent.Option{
+		ent.Debug(),
+		ent.Driver(newDialectDriver(ctx)),
+	}...)
+}
+
+func newDialectDriver(ctx context.Context) dialect.Driver {
+	dbDSN := fmt.Sprintf("%s %s%s", getDSN(os.Getenv("DATABASE_URL")), "options=--search_path=", os.Getenv("DATABASE_SCHEMA"))
+	db, err := sql.Open("postgres", dbDSN)
+	if err != nil {
+		log.Fatal("opening database: ", err)
+	}
+
+	// launch redis client
+	fullURL := os.Getenv("REDIS_URL")
+	pwd := strings.Split(fullURL, "@")[0][9:]
+	addr := strings.Split(fullURL, "@")[1]
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: pwd,
+	})
+	if err := redisClient.Ping(ctx).Err(); err != nil {
+		log.Fatal("redis ping: ", err)
+	}
+
+	entCacheDriver = entcache.NewDriver(
+		dialect.Debug(db),
+		entcache.TTL(ttl),
+		entcache.Levels(
+			entcache.NewLRU(256),
+			entcache.NewRedis(redisClient),
+		),
+	)
+
+	return entCacheDriver
 }
