e4ac267bcdba3219edca645f40734bea2d35ab46
William Vandebelt <wvandebelt@salesforce.com>
2023-04-01 11:58:40 -0400

    clean up

--- init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-read -p "Enter module name: " VALUE
-echo "replacing 'github.com/SFDC/tracks-mono/cmd/template' with:"
-echo "           $VALUE"
-
-rg github.com/SFDC/tracks-mono/cmd/template -l -g '!init.sh' | xargs sed -i -e "s+github.com/SFDC/tracks-mono/cmd/template+$VALUE+g"
-
-rm **/**-e
