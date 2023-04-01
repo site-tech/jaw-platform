f2a485e79645986e69c1d7266f0308ca910c0353
William Vandebelt <wvandebelt@salesforce.com>
2023-04-01 11:06:15 -0400

    removed sailsforce/gomicrokit

--- server.go
+++ server.go
@@ -2,26 +2,25 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/site-tech/jaw-platform/ent"
 	"github.com/site-tech/jaw-platform/graph"
-
-	"github.com/newrelic/go-agent/v3/newrelic"
-
-	kit_utils "github.com/sailsforce/gomicro-kit/utils"
 )
 
 const defaultPort = "8880"
 
 func init() {
-	if err := kit_utils.InitEnv(); err != nil {
+	if err := godotenv.Load(); err != nil {
 		log.Println("error loading .env: ", err)
 	}
 }
@@ -35,7 +34,7 @@ func main() {
 	var entOptions []ent.Option
 	entOptions = append(entOptions, ent.Debug())
 
-	client, err := ent.Open("postgres", kit_utils.GetDSN(os.Getenv("DATABASE_URL")), entOptions...)
+	client, err := ent.Open("postgres", getDSN(os.Getenv("DATABASE_URL")), entOptions...)
 	if err != nil {
 		log.Fatalf("failed opening connection to postgres: %v", err)
 	}
@@ -48,20 +47,32 @@ func main() {
 		}
 	}
 
-	// new relic
-	newRelicApp, err := newrelic.NewApplication(
-		newrelic.ConfigAppName(os.Getenv("NEW_RELIC_APP_NAME")),
-		newrelic.ConfigLicense(os.Getenv("NEW_RELIC_APP_LICENSE")),
-	)
-	if err != nil {
-		log.Fatalf("new relic init failed: %v", err)
-	}
-
 	srv := handler.NewDefaultServer(graph.NewSchema(client))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle(newrelic.WrapHandle(newRelicApp, "/query", srv))
+	http.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func getDSN(url string) string {
+	dsnFormat := "host=%v user=%v password=%v dbname=%v port=%v sslmode=disable"
+	var host string
+	var user string
+	var word string
+	var dbname string
+	var port string
+
+	s1 := strings.Split(url, "://")
+	s2 := strings.Split(s1[1], ":")
+	user = s2[0]
+	s3 := strings.Split(s2[1], "@")
+	word = s3[0]
+	host = s3[1]
+	s4 := strings.Split(s2[2], "/")
+	port = s4[0]
+	dbname = s4[1]
+
+	return fmt.Sprintf(dsnFormat, host, user, word, dbname, port)
 }
